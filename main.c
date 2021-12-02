402bd1928373a371a48108f0a780c77b5d99dbe0
Vincent DURIEZ <vduriez@e1r13p10.42.fr>
2021-12-02 01:07:29 +0100

    on envoit le bout

--- main.c
+++ main.c
@@ -12,6 +12,16 @@
 
 #include "pipex.h"
 
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while(s1[i] && s2[i] && s1[i] == s2[i])
+		i++;
+	return (s1[i] - s2[i]);
+}
+
 void	ft_exec(char *cmd, char **envp)
 {
 	char	**paths;
@@ -24,13 +34,12 @@ void	ft_exec(char *cmd, char **envp)
 		i++;
 	paths = ft_split(envp[i] + 5, ':');
 	i = 0;
+	tmp = ft_split(cmd, ' ');
 	while (paths[i])
 	{
-		path = ft_strjoin(ft_strjoin(paths[i], "/"), cmd);
+		path = ft_strjoin(ft_strjoin(paths[i], "/"), tmp[0]);
 		if (access(path, X_OK) == 0)
 		{
-			//printf("-->%s\n", path);		//! REMOVE
-			tmp = ft_split(cmd, ' ');
 			execve(path, tmp, envp);
 			return ;
 		}
@@ -38,54 +47,69 @@ void	ft_exec(char *cmd, char **envp)
 	}
 }
 
-void	child_process(char **av, int *fd, char **envp)
+char	*get_first_cmd(char** av)
 {
-	int	fdout;
+	char *cmd;
 
-	dup2(fd[1], 1);
-	dup2(fdout, 0);
-	close(fd[0]);
-	ft_exec(av[3], envp);
-	//close(fd[1]);
-	//write(fd[1], cmd, ft_strlen(cmd));
+	cmd = malloc(ft_strlen(av[1]) + ft_strlen(av[2]) + 2);
+	cmd = ft_strjoin(ft_strjoin(av[2], " "), av[1]);
+	return (cmd);
 }
 
-void	parent_process(char **av, int *fd, char **envp)
+void	child_process(char **av, int i, int *fd, char **envp)
 {
-	//char	*str;
 	int		fdin;
+	char	*firstcmd;
 
-	fdin = open(av[4], O_WRONLY | O_CREAT, 0777);
-	//str = malloc(250);
+	dup2(fd[1], 1);
+	dup2(fdin, 0);
+	close(fd[0]);
+	// if (i == 0 && ft_strcmp("cat", av[2]))
+	// {
+	// 	firstcmd = get_first_cmd(av);
+	// 	ft_exec(firstcmd, envp);
+	// }
+	// else
+		ft_exec(av[i + 2], envp);
+}
+
+void	parent_process(char **av, int ac, int *fd, char **envp)
+{
+	int		fdout;
+
+	fdout = open(av[ac - 1], O_WRONLY | O_CREAT | O_TRUNC, 0777);
 	close(fd[1]);
 	dup2(fd[0], 0);
-	dup2(fdin, 1);
-	//read(fd[0], str, 250);
-	//printf("{%s}\n", str);
-	ft_exec(av[3], envp);
-	//close(fd[0]);
+	dup2(fdout, 1);
+	ft_exec(av[ac - 2], envp);
 }		//!  ft_exec("pwd", envp);
 
 int	main(int ac, char**av, char **envp)
 {
 	int		fd[2];
-	int		pid;
+	pid_t	pid[ac - 3];
 	char	*str;
+	int		i;
 
-	ft_exec("ls", envp);
-	if (ac != 5)
+	if (ac < 5)
 		return (0);
 	if (pipe(fd) == -1)
 		exit(1);
-	pid = fork();
-	if (pid < 0)
-		exit(1);
-	if (!pid)
-		child_process(av, fd, envp);
-	else
+	i = 0;
+	while (i < ac - 3)
 	{
-		waitpid(-1, NULL, 0);
-		parent_process(av, fd, envp);
+		pid[i] = fork();
+		if (pid[i] < 0)
+			exit(1);
+		if (!pid[i])
+			child_process(av, i, fd, envp);
+		i++;
+	}
+	i = 0;
+	while (i < ac - 3)
+	{
+		waitpid(pid[i], NULL, 0);
+		parent_process(av, ac, fd, envp);
 	}
 }
 
