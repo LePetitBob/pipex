6e26fb87733ac75e611a4a1758bdd385db38a37d
Vincent DURIEZ <vduriez@e2r5p12.clusters.42paris.fr>
2021-12-06 16:40:08 +0100

    blep

--- bonus/main_bonus.c
+++ bonus/main_bonus.c
@@ -6,7 +6,7 @@
 /*   By: vduriez <vduriez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/26 15:39:40 by vduriez           #+#    #+#             */
-/*   Updated: 2021/12/06 04:43:25 by vduriez          ###   ########.fr       */
+/*   Updated: 2021/12/06 10:22:46 by vduriez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -90,27 +90,21 @@ void	child_process(char **av, int *i, int *fd, char **envp)
 		//dprintf(2, "first\n");
 		fdi = open(av[1], O_RDONLY);
 		//ft_dup2(fdi, fd[1]);
-		dup2(fdi, 0);
-		dup2(fd[1], 1);
-		close(fd[0]);
-		close(fd[1]);
-		close(fdi);
+		dup2(fdi, STDIN_FILENO);
+		dup2(fd[1], STDOUT_FILENO);
 	}
 	else if (i[0] == i[1] - 4)
 	{
 		//dprintf(2, "last\n");
-		fdi = open(av[i[1] - 1], O_WRONLY | O_CREAT | O_TRUNC, 0777);
+		fdi = open(av[i[1] - 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 		//ft_dup2(fd[0], fdi);
-		dup2(fdi, 1);
-		dup2(fd[0], 0);
+		dup2(fdi, STDOUT_FILENO);
+		dup2(fd[0], STDIN_FILENO);
 	}
 	else
 	{
-	//	//ft_dup2(fd[0], fd[1]);
-	//	//dup2(fd[0], 0);
-	//	//dup2(fd[1], 1);
-		dup2(fd[0], 0);
-		dup2(fd[1], 1);
+		dup2(fd[0], STDIN_FILENO);
+		dup2(fd[1], STDOUT_FILENO);
 	}
 	close(fd[1]);
 	close(fd[0]);
@@ -129,18 +123,12 @@ int	main(int ac, char **av, char **envp)
 	if (ac < 5)
 		return (0);
 	i[0] = -1;
-	//while (++i[0] < ac - 4)
-	//{
-	//	if (pipe(fd + 2 * i[0]) < 0)
-	//		exit(1);
-	//}
-	i[0] = -1;
 	i[1] = ac;
 	while (++i[0] < ac - 3)
 	{
-		dprintf(2, "i[0] = %d\n", i[0]);
 		if (pipe(fd) < 0)
 			exit(1);
+		printf("i[0] = %d\n", i[0]);
 		pid[i[0]] = fork();
 		if (pid[i[0]] < 0)
 			exit(1);
@@ -155,7 +143,7 @@ int	main(int ac, char **av, char **envp)
 	return (0);
 }
 
-//! -----------------------KEEPSAFE------------------------!//
+//!------------------------KEEPSAFE------------------------!//
 //void	child_process(char **av, int *i, int *fd, char **envp)
 //{
 //	int	fdi;
